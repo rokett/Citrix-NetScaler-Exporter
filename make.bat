129e729293b109eb314b7e76140ce5735ece5e5f
Rokett <rokett@rokett.me>
2019-06-15 19:59:42 +0100

    Reducing build size

--- make.bat
+++ make.bat
@@ -6,16 +6,14 @@ set _TARGETS=build
 set APP=Citrix-NetScaler-Exporter
 set VERSION=3.2.0
 set BINARY-WINDOWS-X64=%APP%_%VERSION%_Windows_amd64.exe
-set BINARY-LINUX=%APP%_%VERSION%_amd64
+set BINARY-LINUX=%APP%_%VERSION%_Linux_amd64
 
 REM Set build number from git commit hash
 for /f %%i in ('git rev-parse HEAD') do set BUILD=%%i
 
 if [%1]==[] goto build
 
-REM *** CHECK THAT VALID ARG IS PASSED ***
-
-set LDFLAGS=-ldflags "-X main.version=%VERSION% -X main.build=%BUILD%"
+set LDFLAGS=-ldflags "-X main.version=%VERSION% -X main.build=%BUILD% -s -w"
 
 goto %1
 
