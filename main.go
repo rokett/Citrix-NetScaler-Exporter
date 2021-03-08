366a692acb2418f01a1e87b04d12cfe85ff5f128
Greg Perry <greg.perry@encore.tech>
2021-03-08 14:59:25 -0500

    lower casing

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/EncoreTechnologies/citrix-netscaler-exporter/collector"
+	"github.com/encoretechnologies/citrix-netscaler-exporter/collector"
 )
 
 var (
